316903ef7555a6e5ae2f4c0820a2a402fea16b93
manomelo <lucas.melo2103@gmail.com>
2026-03-11 14:11:02 -0300

    Estrutura basica criada

---
+++ src/main/java/projeto/loja/lojabijuteria/LojabijuteriaApplication.java
@@ -0,0 +1,13 @@
+package projeto.loja.lojabijuteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojabijuteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojabijuteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/loja/lojabijuteria/LojabijuteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.loja.lojabijuteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojabijuteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
